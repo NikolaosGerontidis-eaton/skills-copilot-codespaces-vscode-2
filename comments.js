ad6f5df1ceaaf2dc123e01aca58525ba94a8925f
NikolaosGerontidis-eaton <106660055+NikolaosGerontidis-eaton@users.noreply.github.com>
2024-04-03 14:48:27 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,71 @@
+// create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// create web server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
+// create comments
+var comments = require('./comments.json');
+var fs = require('fs');
+
+// get comments
+function getComments() {
+    return comments;
+}
+
+// add comment
+function addComment(comment) {
+    comments.push(comment);
+    fs.writeFileSync('comments.json', JSON.stringify(comments));
+}
+
+// export functions
+module.exports = {
+    getComments: getComments,
+    addComment: addComment
+};
+
+// Path: index.js
+// create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var comments = require('./comments');
+
+// create web server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
+// get comments
+console.log(comments.getComments());
+
+// add comment
+comments.addComment('Hello!');
+
+// get comments
+console.log(comments.getComments());
