9d1685621e0b419ead8bb3fff040237e8f56e862
NikolaosGerontidis-eaton <106660055+NikolaosGerontidis-eaton@users.noreply.github.com>
2024-04-03 15:01:37 +0000

    Typo

--- comments.js
+++ comments.js
@@ -1,9 +1,9 @@
-// create web server
+// Create web server
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-// create web server
+// Create web server
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
